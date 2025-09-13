236b8e9adf410554749c04ebd1f656cc6fc659c5
Opop29 <joshuaquidit123456@gmail.com>
2025-09-13 13:58:51 +0800

    add logout in home button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,23 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
 import React from "react";
+import { IonPage, IonContent, IonButton } from "@ionic/react";
+import { useHistory } from "react-router-dom";
+
 const Home: React.FC = () => {
+  const history = useHistory();
+
+  const handleLogout = () => {
+    history.push("/Toltul-ad/enter");
+  };
+
   return (
     <IonPage>
       <IonContent className="ion-padding ion-text-center">
         <h1>🎉 Welcome to Home Page</h1>
         <p>You have successfully entered the passcode!</p>
+
+        <IonButton expand="block" color="danger" onClick={handleLogout}>
+          🚪 Logout
+        </IonButton>
       </IonContent>
     </IonPage>
   );
