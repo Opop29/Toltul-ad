885a880290fc4e409815cd775b67ec7fe697c7b1
Opop29 <joshuaquidit123456@gmail.com>
2025-09-16 21:28:03 +0800

    Updates

--- assets/ios.transition-legacy-DpZpZPWt.js
+++ assets/ios.transition-legacy-DpZpZPWt.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DUB_P9iC.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-ByLoLApl.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.k,n=t.l}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/keyboard-legacy-Ci8WxhFA.js
+++ assets/keyboard-legacy-Ci8WxhFA.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DUB_P9iC.js"],function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-ByLoLApl.js"],function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
