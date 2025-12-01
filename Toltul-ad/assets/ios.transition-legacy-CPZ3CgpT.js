1269a6ab71a9566ebb7f9c0deabb0081a8056093
Opop29 <joshuaquidit123456@gmail.com>
2025-12-02 01:04:11 +0800

    Updates

--- Toltul-ad/assets/ios.transition-legacy-CPZ3CgpT.js
+++ Toltul-ad/assets/ios.transition-legacy-CPZ3CgpT.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CnKo7i-X.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-BnPPr5Kd.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
