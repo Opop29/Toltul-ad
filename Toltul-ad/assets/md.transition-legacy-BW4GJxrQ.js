493093ae5a1b35cc47e2812a3f2a75f95cd84f5a
Opop29 <joshuaquidit123456@gmail.com>
2025-12-02 00:25:12 +0800

    Updates

--- Toltul-ad/assets/md.transition-legacy-BW4GJxrQ.js
+++ Toltul-ad/assets/md.transition-legacy-BW4GJxrQ.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-7auU8BrC.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-BbL0KZnF.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- Toltul-ad/assets/status-tap-legacy-t14m9bMq.js
+++ Toltul-ad/assets/status-tap-legacy-t14m9bMq.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-7auU8BrC.js"],function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-BbL0KZnF.js"],function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
